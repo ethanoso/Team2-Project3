3135545f7127656fd2b3841be2a8463363bde488
adhorton00 <83788029+adhorton00@users.noreply.github.com>
2022-04-03 19:45:50 -0500

    Final update

--- data.js
+++ data.js
@@ -1,13 +1,1 @@
 // file to store answer from quiz page to display on quiz results page
-let value = {
-  firstValue: 0,
-  secondValue: 0,
-  setFirstValue(value)
-  {
-    this.firstValue = value;
-  },
-  getFirstValue()
-  {
-    return this.firstValue;
-  }
-};
