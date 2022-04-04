0b5446c433ac567f79a87ea3bd737278dbfff270
adhorton00 <83788029+adhorton00@users.noreply.github.com>
2022-04-03 19:48:54 -0500

    final update

--- instrumentquizresult.js
+++ instrumentquizresult.js
@@ -1,3 +1,8 @@
+/*
+instrumentquizresult.js
+js class for instrument quiz result page, stores instrument item into session for retrieval by instrumentquizresult class.
+Class was modified from the following published code: https://github.com/RajnishKatharotiya/share-data-between-html-pages
+*/
 window.addEventListener('load', () => {
 
   value1 = sessionStorage.getItem('VALUE1');
